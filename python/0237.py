959229cb789ed60244256ac5c94e8e838957707d
garywei944 <33930674+garywei944@users.noreply.github.com>
2022-01-07 14:12:30 -0500

    0237

--- python/0237.py
+++ python/0237.py
@@ -1,8 +1,6 @@
 from leetcode_tester import Tester
 from python.src.list_node import ListNode
 
-from typing import Optional, List
-
 
 class Solution:
     def deleteNode(self, node):
@@ -10,14 +8,30 @@ class Solution:
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
-        pass
+        node.val = node.next.val
+        node.next = node.next.next
+
+    # For test use
+    def tester(self, head, node):
+        self.deleteNode(node)
+
+        return head
 
 
 if __name__ == '__main__':
     solution = Solution()
-    test = Tester(solution.deleteNode)
 
+    test = Tester(solution.tester)
+
+    head = ListNode.from_list([4, 5, 1, 9])
+    node = head.next  # 5
     test.addTest(
-        None
+        head, node, ListNode.from_list([4, 1, 9])
+    )
+
+    head = ListNode.from_list([4, 5, 1, 9])
+    node = head.next.next  # 1
+    test.addTest(
+        head, node, ListNode.from_list([4, 5, 9])
     )
     test.doTest()
